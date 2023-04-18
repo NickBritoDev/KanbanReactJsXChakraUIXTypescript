d285f84edb189aad8ad6dc15c29f34811e79f10a
NickBritoDev <nicolasbcruz@gmail.com>
2023-04-18 01:53:58 -0300

    mudanças no deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
-
-module.exports = {
   build: {
-    base: '/KanbanReactJsXChakraUIXTypescript/', 
+    outDir: 'dist',
+    assetsDir: '',
+    rollupOptions: {
+      input: 'index.html',
+    },
   },
-}
+  base: 'https://github.com/NickBritoDev/KanbanReactJsXChakraUIXTypescript/', 
+});
